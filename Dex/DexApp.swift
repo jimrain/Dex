559fae9bf6042171acc46aeed7473e4ea3010f60
jimrain <jim@rainville.net>
2025-02-26 05:40:12 -0800

    Initial Commit

---
+++ Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Jim Rainville on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
