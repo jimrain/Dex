febc63635473ae0206f70fc2e2cff88b59498a87
jimrain <jim@rainville.net>
2025-03-20 06:25:41 -0700

    Lesson 19 - Added widget extension.

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Jim Rainville on 3/20/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
